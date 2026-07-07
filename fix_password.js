de47895b88fa8968a2ac8ffc9bcf6b8243b584e9
mariaclaramodas618-pixel <mariaclaramodas618-pixel@users.noreply.github.com>
2026-07-06 22:13:00 -0300

    fix: localStorage token key mismatch in index.html

--- fix_password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
-
-async function run() {
-    const hash = await bcrypt.hash('Admin@2026', 10);
-    console.log("NOVO HASH: " + hash);
-}
-
-run();
